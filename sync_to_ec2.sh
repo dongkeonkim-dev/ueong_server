6c8db63eb527d1c20b5adb4e53c47841fd465270
testGit <extraksw@naver.com>
2024-10-26 20:52:56 +0900

    dasf

---
+++ sync_to_ec2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# PEM 파일 경로
+PEM_FILE_PATH="/Users/mymac/.ssh/ueong_server_key.pem"
+
+# 로컬 프로젝트 경로
+LOCAL_PROJECT_PATH="/Users/mymac/Desktop/capstone/ueong_server"
+
+# EC2 접속 정보
+EC2_USER="ec2-user"
+EC2_DNS="ec2-43-200-8-246.ap-northeast-2.compute.amazonaws.com"
+EC2_DEST_PATH="/home/ec2-user"
+
+# rsync 명령어
+rsync -avz -e "ssh -i $PEM_FILE_PATH" $LOCAL_PROJECT_PATH $EC2_USER@$EC2_DNS:$EC2_DEST_PATH
+
